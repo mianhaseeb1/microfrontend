95393dc7cee5f42deccceabd86bb51d2855d50d3
IsmarB <ismar.begic@enetelsolutions.com>
2024-02-07 21:26:53 +0100

    minor placeholder change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  pagesApi: 'https://your-production-page-api-url/',
-  authApi: 'https://your-production-auth-api-url/',
+  pagesApi: 'https://auxee-page-microservice.azurewebsites.net/v1/pages/',
+  authApi: 'https://auxee-dev-gateway.azurewebsites.net/api/',
 };
