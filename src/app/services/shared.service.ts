2aed2772ff5e12f207fdad1ebc0a2e86ffc9898e
IsmarB <ismar.begic@enetelsolutions.com>
2024-02-13 00:33:25 +0100

    refactor

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -7,10 +7,16 @@ import { PagesData } from '../models/pages-cards.model';
 })
 export class SharedService {
   private dataSource = new BehaviorSubject<PagesData | null>(null);
+  private titleSource = new BehaviorSubject<string | null>(null);
+
   currentData = this.dataSource.asObservable();
+  currentTitle = this.titleSource.asObservable();
 
   updateData(data: PagesData | null) {
     this.dataSource.next(data);
   }
 
+  updateTitle(title: string | null) {
+    this.titleSource.next(title);
+  }
 }
